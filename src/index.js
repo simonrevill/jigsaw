cdca0134e6c8eba04c33849824ca25f57acf03fb
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2020-05-01 20:21:49 +0100

    remove create-react-app boilerplate code. start redux store / folder setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
