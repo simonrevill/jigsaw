899c53ec22b30623d7685441c054723230cd2101
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2020-05-05 11:46:49 +0100

    added dark mode slider / styles

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Slider = () => {
-  return (
-    <div>
+  const [isOn, setOn] = useState(false);
 
-    </div>
+  const handleClick = () => {
+    setOn(!isOn);
+  };
+
+  return (
+    <React.Fragment>
+      <div className="slider__label">Dark Mode:</div>
+      <div className={isOn ? "slider slider--active" : "slider"} onClick={handleClick}>
+        <div className="slider__inner">
+          <div className="slider__switch"></div>
+        </div>
+      </div>
+      <div className={isOn ? "slider__state slider__state--active" : "slider__state"}>
+        {isOn ? "On" : "Off"}
+      </div>
+    </React.Fragment>
   );
 };
 
