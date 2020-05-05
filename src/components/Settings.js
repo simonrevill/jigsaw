bde66efe58017bad4951469d01734efd7478d234
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2020-05-05 21:32:14 +0100

    addded Settings component / styles / sliders

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -5,9 +5,12 @@ import Slider from './Slider';
 function Settings() {
   return (
     <div className="main__settings">
-      <Slider sliderLabel="Show background image" showSliderState={false} />
-      <Slider sliderLabel="Show grid overlay" showSliderState={false} />
-      <Slider sliderLabel="Show correct placement" showSliderState={false} />
+      <div className="main__inner">
+        <h2 className="main__title">Difficulty</h2>
+        <Slider sliderLabel="Show background image" showSliderState={false} />
+        <Slider sliderLabel="Show grid overlay" showSliderState={false} />
+        <Slider sliderLabel="Show correct placement" showSliderState={false} />
+      </div>
     </div>
   );
 };
