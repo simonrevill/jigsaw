be965795201382e8ff4d8076a5713f1c2f694502
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2020-05-05 21:34:57 +0100

    added missing ':'

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -6,7 +6,7 @@ function Settings() {
   return (
     <div className="main__settings">
       <div className="main__inner">
-        <h2 className="main__title">Difficulty</h2>
+        <h2 className="main__title">Difficulty:</h2>
         <Slider sliderLabel="Show background image" showSliderState={false} />
         <Slider sliderLabel="Show grid overlay" showSliderState={false} />
         <Slider sliderLabel="Show correct placement" showSliderState={false} />
