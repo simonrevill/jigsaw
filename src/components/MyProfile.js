7c11f128caf673a6bf76c22d20b8c45ad9890059
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2020-05-09 17:29:20 +0100

    started building MyProfile panel

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import '../scss/bem/MyProfile.scss';
+
 const MyProfile = ({ isActive }) => {
   return (
-    <div className={isActive ? 'my-profile d-block' : 'my-profile d-none'}>
+    <div className={isActive ? 'my-profile d-flex' : 'my-profile d-none'}>
+      <div className="my-profile__card"></div>
+      <div className="my-profile__favourites"></div>
 
     </div>
   );
