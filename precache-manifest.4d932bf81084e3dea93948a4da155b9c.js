27cf8c6f38f5597e7016d2a5a6931fe1015f735b
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2020-05-11 21:30:26 +0100

    Updates

---
+++ precache-manifest.4d932bf81084e3dea93948a4da155b9c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb56bf902fe9053fe2a2230be175bbd1",
+    "url": "/jigsaw/index.html"
+  },
+  {
+    "revision": "11ffce1cc9aadc927fc7",
+    "url": "/jigsaw/static/css/main.48aff9fc.chunk.css"
+  },
+  {
+    "revision": "8c0bc40d7db556a325a5",
+    "url": "/jigsaw/static/js/2.b2c96707.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jigsaw/static/js/2.b2c96707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11ffce1cc9aadc927fc7",
+    "url": "/jigsaw/static/js/main.c4a4183f.chunk.js"
+  },
+  {
+    "revision": "2e5f6a62a1cc70861a0f",
+    "url": "/jigsaw/static/js/runtime-main.49702581.js"
+  },
+  {
+    "revision": "8504a8e9636594872be2b7e4ddc9676a",
+    "url": "/jigsaw/static/media/Comfortaa-Bold.8504a8e9.ttf"
+  },
+  {
+    "revision": "c2488bfda16e6a811f62414da90e51e4",
+    "url": "/jigsaw/static/media/Comfortaa-Regular.c2488bfd.ttf"
+  },
+  {
+    "revision": "59f36286973a52baec718ba6e3f0cbb6",
+    "url": "/jigsaw/static/media/board.59f36286.svg"
+  },
+  {
+    "revision": "5bfdd8b6d1952962fd354e164295ff25",
+    "url": "/jigsaw/static/media/connect-line.5bfdd8b6.svg"
+  },
+  {
+    "revision": "dcdbaf161743e98a9ed16b2c639ebff5",
+    "url": "/jigsaw/static/media/connect-separator.dcdbaf16.svg"
+  },
+  {
+    "revision": "961547a6c00e8aaaf2e1c9e699fb36d3",
+    "url": "/jigsaw/static/media/connect-step.961547a6.svg"
+  },
+  {
+    "revision": "c2790743147d77f63a4d084039cf83d2",
+    "url": "/jigsaw/static/media/dot.c2790743.svg"
+  },
+  {
+    "revision": "ade360e5cdca403e8db7bcdbd4471305",
+    "url": "/jigsaw/static/media/heart-solid.ade360e5.svg"
+  },
+  {
+    "revision": "f04d44a8c5698cb9cf59411d5930af82",
+    "url": "/jigsaw/static/media/johnSmith.f04d44a8.jpg"
+  },
+  {
+    "revision": "4f902cd96990f7b9e8e6ab661e775af9",
+    "url": "/jigsaw/static/media/library.4f902cd9.svg"
+  },
+  {
+    "revision": "edbf9c5f588931131dd4e7edf09a97c3",
+    "url": "/jigsaw/static/media/marySmith.edbf9c5f.jpg"
+  },
+  {
+    "revision": "f0a47e1dc62a941da7401cbdcf15aaee",
+    "url": "/jigsaw/static/media/menu-close.f0a47e1d.svg"
+  },
+  {
+    "revision": "53e3a30e813c80408164fa0f6e9259c2",
+    "url": "/jigsaw/static/media/menu-open.53e3a30e.svg"
+  },
+  {
+    "revision": "2191dfa2445d03be595c315d1fa73760",
+    "url": "/jigsaw/static/media/settings.2191dfa2.svg"
+  },
+  {
+    "revision": "9b899c9460e74cddca2e7cec05f9e654",
+    "url": "/jigsaw/static/media/upload-drag-and-drop.9b899c94.svg"
+  },
+  {
+    "revision": "2e4df2eb490010a3693e1e3c613348fa",
+    "url": "/jigsaw/static/media/upload.2e4df2eb.svg"
+  }
+]);
