7cd5f36e7346b5eecb013e93213ebfc3746b675f
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2020-05-12 22:34:50 +0100

    installed redux-devtools-extension package as dev-dependency

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers/rootReducer';
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools()
 );
 
 export default store;
