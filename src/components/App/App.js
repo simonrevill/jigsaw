a9d8d968a5d668467ba9905d933a1d20729aaf29
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2020-05-27 10:32:28 +0100

    moved AWS services into App component, ready for updating Redux store.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,46 @@
 import React from 'react';
-
 import { connect } from 'react-redux';
 import { getMenuIsOpen, getTabs } from '../../redux/selectors/uiState';
 import { getCurrentUserInfo } from '../../redux/selectors/users';
-
 import Topbar from '../Topbar/Topbar';
 import Sidebar from '../Sidebar/Sidebar';
 import Main from '../Main/Main';
-
 import '../../scss/bem/App.scss';
+import AWS from 'aws-sdk';
+import config from '../../aws/config';
+
+AWS.config = config;
+
+// S3 Storage:
+
+const s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  params: { Bucket: 'jigsaw-image-library' }
+});
+
+s3.listObjectsV2(function (err, data) {
+  if (err) console.log('There was an error listing your objects: ' + err.message);
+  // const href = this.request.httpRequest.endpoint.href;
+  // const bucketUrl = `${href}${bucketName}/`;
+  // const imagesFolderUrl = `${bucketUrl}images/`;
+  console.log(data);
+});
+
+// DynamoDB database:
+
+const userId = 'f3a0f858-57b4-4420-81fa-1f0acdec979d';
+const tableName = 'registeredUsers';
+const docClient = new AWS.DynamoDB.DocumentClient();
+const params = {
+  TableName: tableName,
+  Key: { userId }
+};
+
+docClient.get(params, (err, data) => {
+  err ?
+    console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2)) :
+    console.log(JSON.stringify(data, null, 2));
+});
 
 const App = ({ currentUserInfo, menuIsOpen, tabs }) => {
   return (
