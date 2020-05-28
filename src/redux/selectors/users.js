f65ae1cb83bd3c3edb3678b20cd28c76cb67199e
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2020-05-28 14:52:26 +0100

    tidy up selectors with destructuring

--- src/redux/selectors/users.js
+++ src/redux/selectors/users.js
@@ -1,29 +1,29 @@
 export const getCurrentUser = store => store.users.currentUser;
 
 export const getCurrentUserInfo = store => {
-  const currentUserInfo = store.users.currentUserInfo;
+  const { currentUserInfo } = store.users;
   return currentUserInfo;
 };
 
 export const getUserPreferences = store => {
-  const userPreferences = store.users.currentUserInfo.userPreferences;
+  const { userPreferences } = store.users.currentUserInfo;
   return userPreferences;
 };
 
 export const getUserShowBackgroundImage = store => {
   const userPreferences = getUserPreferences(store);
-  const showBackgroundImage = userPreferences.difficulty.showBackgroundImage;
+  const { showBackgroundImage } = userPreferences.difficulty;
   return showBackgroundImage;
 };
 
 export const getUserShowGridOverlay = store => {
   const userPreferences = getUserPreferences(store);
-  const showGridOverlay = userPreferences.difficulty.showGridOverlay;
+  const { showGridOverlay } = userPreferences.difficulty;
   return showGridOverlay;
 };
 
 export const getUserShowCorrectPlacement = store => {
   const userPreferences = getUserPreferences(store);
-  const showCorrectPlacement = userPreferences.difficulty.showCorrectPlacement;
+  const { showCorrectPlacement } = userPreferences.difficulty;
   return showCorrectPlacement;
 };
