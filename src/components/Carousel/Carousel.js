ed11d683bf17af825d40a415038fa97edef19df8
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2020-06-14 15:23:54 +0100

    added carousel browser panel. improved hover states on sidebar and other buttons and tabs, similar to the grid setup area

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { ReactComponent as CarouselButtonPrev } from '../../icons/carousel-button-prev.svg';
+import { ReactComponent as CarouselButtonNext } from '../../icons/carousel-button-next.svg';
 import '../../scss/bem/Carousel.scss';
 
 const Carousel = () => {
   return (
-    <div className="carousel"></div>
+    <div className="carousel">
+      <div className="carousel__browser"></div>
+      <div className="carousel__browser-panel">
+        <div className="carousel__button carousel__button--prev">
+          <CarouselButtonPrev />
+        </div>
+        <div className="carousel__file-name">
+          <p className="carousel__file-name-text">tower-restaurant.jpg</p>
+        </div>
+        <div className="carousel__button carousel-button--next">
+          <CarouselButtonNext />
+        </div>
+      </div>
+    </div>
   );
 };
 
