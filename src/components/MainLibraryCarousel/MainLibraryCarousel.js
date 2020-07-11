f5b846c0cf88e082595a0b922da75da5cadc6cff
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2020-07-11 11:29:24 +0100

    Added the react-transition-group addon, using it for transition classes applied to the grid overlays in the Carousel.

--- src/components/MainLibraryCarousel/MainLibraryCarousel.js
+++ src/components/MainLibraryCarousel/MainLibraryCarousel.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { SwitchTransition, CSSTransition } from 'react-transition-group';
 import AliceCarousel from 'react-alice-carousel';
 import { ReactComponent as CarouselButtonPrev } from '../../icons/carousel-button-prev.svg';
 import { ReactComponent as CarouselButtonNext } from '../../icons/carousel-button-next.svg';
+import { ReactComponent as GridOverlay4 } from '../../icons/grid-overlay-4.svg';
+import { ReactComponent as GridOverlay5 } from '../../icons/grid-overlay-5.svg';
+import { ReactComponent as GridOverlay6 } from '../../icons/grid-overlay-6.svg';
+import { ReactComponent as GridOverlay7 } from '../../icons/grid-overlay-7.svg';
 import { ReactComponent as GridOverlay8 } from '../../icons/grid-overlay-8.svg';
 import '../../../node_modules/react-alice-carousel/lib/alice-carousel.css';
 
@@ -11,8 +16,27 @@ class MainLibraryCarousel extends React.Component {
 
   render() {
 
-
     const { gridSetting, imageLibrary, currentSelectedImage, handlePreviousClick, handleNextClick } = this.props;
+
+    const renderGridOverlay = () => {
+      switch (gridSetting) {
+        case 0:
+          return <div key={gridSetting}></div>;
+        case 4:
+          return <GridOverlay4 key={gridSetting} />;
+        case 5:
+          return <GridOverlay5 key={gridSetting} />;
+        case 6:
+          return <GridOverlay6 key={gridSetting} />;
+        case 7:
+          return <GridOverlay7 key={gridSetting} />;
+        case 8:
+          return <GridOverlay8 key={gridSetting} />;
+        default:
+          return <div key={gridSetting}></div>;
+      }
+    };
+
 
     const currentIndex = imageLibrary.findIndex(image => image.id === currentSelectedImage.id);
 
@@ -43,7 +67,27 @@ class MainLibraryCarousel extends React.Component {
           ref={(el) => (this.Carousel = el)}
         />
         <div className="carousel__grid-overlay">
-          <GridOverlay8 />
+          <SwitchTransition>
+            <CSSTransition
+              key={gridSetting}
+              in={true}
+              appear
+              timeout={75}
+              classNames={{
+                appear: 'carousel__grid-overlay-grid--appear',
+                appearActive: 'carousel__grid-overlay-grid--appear-active',
+                appearDone: 'carousel__grid-overlay-grid--appear-done',
+                enter: 'carousel__grid-overlay-grid--enter',
+                enterActive: 'carousel__grid-overlay-grid--enter-active',
+                enterDone: 'carousel__grid-overlay-grid--enter-done',
+                exit: 'carousel__grid-overlay-grid--exit',
+                exitActive: 'carousel__grid-overlay-grid--exit-active',
+                exitDone: 'carousel__grid-overlay-grid--exit-done',
+              }}
+            >
+              {renderGridOverlay(gridSetting)}
+            </CSSTransition>
+          </SwitchTransition>
         </div>
         <div className="carousel__browser-panel">
           <div
