06924314dbd6e8748bb7a9fccd7402b8a6043a2c
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2020-07-20 12:53:32 +0100

    File Info destructuring. More alignment fixes on rating stars.

--- src/components/FileInfo/FileInfo.js
+++ src/components/FileInfo/FileInfo.js
@@ -6,30 +6,32 @@ import { ReactComponent as HeartSolid } from '../../icons/heart-solid.svg';
 import '../../scss/bem/FileInfo.scss';
 
 const FileInfo = ({ currentSelectedImage }) => {
+
+  const { name, rating } = currentSelectedImage;
+
   return (
     <div className="file-info">
       <div className="file-info__file-name">
         <p className="file-info__file-name-title">File name:</p>
-        <p className="file-info__file-name-text">{currentSelectedImage.name}</p>
+        <p className="file-info__file-name-text">{name}</p>
       </div>
       <div className="file-info__rating">
         <p className="file-info__rating-title">Rating:</p>
         <div className="file-info__rating-stars">
-          {currentSelectedImage.rating}
           <StarSolid
-            className="file-info__rating-star-solid"
+            className="file-info__rating-star file-info__rating-star--solid"
           />
           <StarSolid
-            className="file-info__rating-star-solid"
+            className="file-info__rating-star file-info__rating-star--solid"
           />
           <StarSolid
-            className="file-info__rating-star-solid"
+            className="file-info__rating-star file-info__rating-star--solid"
           />
           <Star
-            className="file-info__rating-star"
+            className="file-info__rating-star file-info__rating-star--empty"
           />
           <Star
-            className="file-info__rating-star"
+            className="file-info__rating-star file-info__rating-star--empty"
           />
         </div>
       </div>
